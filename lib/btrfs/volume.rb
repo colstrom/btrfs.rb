d0148125589568350be52ca1cb7e400b46a5edf3
Chris Olstrom <chris@olstrom.com>
2016-09-13 16:21:39 -0700

    Add simple object for volumes

---
+++ lib/btrfs/volume.rb
@@ -0,0 +1,18 @@
+require_relative 'subvolume'
+
+module Btrfs
+  class Volume
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+      self
+    end
+
+    def subvolumes
+      `btrfs subvolume list -t #{path}`.lines.drop(2).map do |subvolume|
+        Subvolume.new path, line
+      end
+    end
+  end
+end
